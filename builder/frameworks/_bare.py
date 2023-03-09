e80972cd7f1337cde874e13df1b5eab80a7fb26c
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-03-09 14:55:15 +0300

    Revert "Update _bare.py"
    This reverts commit 699396fbddc17f8d7e68f95152d45713e8b848c2.

--- builder/frameworks/_bare.py
+++ builder/frameworks/_bare.py
@@ -12,16 +12,12 @@ env.AppendUnique(
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
-        "-nostartfiles",
-        "-nostdlib",
     ],
 
     CCFLAGS=[
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
-        "-nostartfiles",
-        "-nostdlib",
         "-ffunction-sections",
     ],
 
@@ -32,7 +28,6 @@ env.AppendUnique(
         "-Xlinker",
         "-Map=%s.map" % join("$BUILD_DIR", "${PROGNAME}"),
         "-nostartfiles",
-        "-nostdlib",
         "-Wl,--gc-sections",
     ],
 )
