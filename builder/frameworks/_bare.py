7a9758ebb27aa03812c2484b84d32f6007d4e66b
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-03-09 14:55:37 +0300

    Revert "add ffunction-sections and gc-sections by default"
    This reverts commit 1a48aad1b12ab29ca55da37554e08b210a43ed8c.

--- builder/frameworks/_bare.py
+++ builder/frameworks/_bare.py
@@ -18,7 +18,6 @@ env.AppendUnique(
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
-        "-ffunction-sections",
     ],
 
     LINKFLAGS=[
@@ -28,6 +27,5 @@ env.AppendUnique(
         "-Xlinker",
         "-Map=%s.map" % join("$BUILD_DIR", "${PROGNAME}"),
         "-nostartfiles",
-        "-Wl,--gc-sections",
     ],
 )
