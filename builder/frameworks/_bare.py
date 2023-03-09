1a48aad1b12ab29ca55da37554e08b210a43ed8c
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-03-09 10:32:50 +0300

    add ffunction-sections and gc-sections by default

--- builder/frameworks/_bare.py
+++ builder/frameworks/_bare.py
@@ -18,6 +18,7 @@ env.AppendUnique(
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
+        "-ffunction-sections",
     ],
 
     LINKFLAGS=[
@@ -27,5 +28,6 @@ env.AppendUnique(
         "-Xlinker",
         "-Map=%s.map" % join("$BUILD_DIR", "${PROGNAME}"),
         "-nostartfiles",
+        "-Wl,--gc-sections",
     ],
 )
