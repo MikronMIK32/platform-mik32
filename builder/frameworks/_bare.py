699396fbddc17f8d7e68f95152d45713e8b848c2
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-03-09 12:34:37 +0300

    Update _bare.py

--- builder/frameworks/_bare.py
+++ builder/frameworks/_bare.py
@@ -12,12 +12,16 @@ env.AppendUnique(
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
+        "-nostartfiles",
+        "-nostdlib",
     ],
 
     CCFLAGS=[
         "-march=%s" % board_config.get("build.march"),
         "-mabi=%s" % board_config.get("build.mabi"),
         "-mcmodel=%s" % board_config.get("build.mcmodel"),
+        "-nostartfiles",
+        "-nostdlib",
         "-ffunction-sections",
     ],
 
@@ -28,6 +32,7 @@ env.AppendUnique(
         "-Xlinker",
         "-Map=%s.map" % join("$BUILD_DIR", "${PROGNAME}"),
         "-nostartfiles",
+        "-nostdlib",
         "-Wl,--gc-sections",
     ],
 )
